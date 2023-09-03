4329e7464876bfb34dd72d57d4a63f13a3d912f3
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-09-03 23:00:16 +0530

    added enums_and patterns

---
+++ enums_and_pattern/target/debug/deps/enums_and_pattern-5f156183f44d4cde.d
@@ -0,0 +1,5 @@
+E:\Github\RustDocsHandsOn\enums_and_pattern\target\debug\deps\enums_and_pattern-5f156183f44d4cde.exe: src\main.rs
+
+E:\Github\RustDocsHandsOn\enums_and_pattern\target\debug\deps\enums_and_pattern-5f156183f44d4cde.d: src\main.rs
+
+src\main.rs:
---
+++ enums_and_pattern/target/debug/enums_and_pattern.d
@@ -0,0 +1,1 @@
+E:\Github\RustDocsHandsOn\enums_and_pattern\target\debug\enums_and_pattern.exe: E:\Github\RustDocsHandsOn\enums_and_pattern\src\main.rs
