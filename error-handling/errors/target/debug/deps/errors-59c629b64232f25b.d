275fdb3f687c08e21e9d3ea7bfebb85f98953127
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-09-10 16:34:21 +0530

    added erro-handling

---
+++ error-handling/errors/target/debug/deps/errors-59c629b64232f25b.d
@@ -0,0 +1,5 @@
+E:\Github\RustDocsHandsOn\error-handling\errors\target\debug\deps\errors-59c629b64232f25b.exe: src\main.rs
+
+E:\Github\RustDocsHandsOn\error-handling\errors\target\debug\deps\errors-59c629b64232f25b.d: src\main.rs
+
+src\main.rs:
---
+++ error-handling/errors/target/debug/errors.d
@@ -0,0 +1,1 @@
+E:\Github\RustDocsHandsOn\error-handling\errors\target\debug\errors.exe: E:\Github\RustDocsHandsOn\error-handling\errors\src\main.rs
