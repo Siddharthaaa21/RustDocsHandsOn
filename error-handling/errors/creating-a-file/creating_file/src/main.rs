783cd263cb8e1015dc64dd6a1f20b937b824dc0d
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-09-11 10:48:26 +0530

    error handling in Rust

--- error-handling/errors/creating-a-file/creating_file/src/main.rs
+++ error-handling/errors/creating-a-file/creating_file/src/main.rs
@@ -17,3 +17,25 @@ let f = match f {
     }
 };
 }
+/* simplifying the code using closures */
+/* fn main(){
+    let f=File::opem("hello.txt").expect(""failed to open hello.txt");
+
+}
+/* to further simplifying this code!!
+use std::fs::File;
+use std::io;
+use st::io::Read;
+
+fn read_username_from_file()-> Result<String,io::error>{
+    let mut s=String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+fn main(){
+
+
+}
+fn read_username_form_file()->Reult<String,io::Error>{
+    fs::read_to_string("hello.txt")//import ==>>use std::fs::{self,File};
+}
