eaa47f2ddde3aa129ca376c682dd85a67ee32cf7
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-09-26 23:39:46 +0530

    pushed on cli application

--- Mini-Grep/minigrep/src/main.rs
+++ Mini-Grep/minigrep/src/main.rs
@@ -6,11 +6,21 @@ fn main(){
   println!("{:?}", args );//{:?} is used to print the vector of string
 
 //store query and call it now
-let query=&args[1];
-let filename=&args[2];
-println!("Searching for {}", query);
-println!("in file {}", filename);
-let content = fs::read_to_string(filename).expect("something went wrong");
+let config=parse_config(&args);
+println!("Searching for {}", config.query);
+println!("in file {}", config.filename);
+let content = fs::read_to_string(config.filename).expect("something went wrong");
 println!("With text:\n{}", content);
+
+}
+struct Config{
+  query:String,
+  filename:String,
+}
+
+fn parse_config(args:&[String ]) -> Config{
+  let query=args[1].clone();
+  let filename=args[2].clone();
+  Config{query,filename}
 
 }
